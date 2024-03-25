4f4cd0e3e6096c6143a4ce4fc0638a14d5c25157
Greenwood <greenwood.ts@gmail.com>
2024-03-24 19:09:24 -0700

    Update mapping_functions.py
    Set min_zoom

--- kingcountytransitmapping/utils/mapping_functions.py
+++ kingcountytransitmapping/utils/mapping_functions.py
@@ -2,11 +2,11 @@ import folium
 from folium import plugins
 
 def create_kc_map(location=[47.608013, -122.335167], 
-                  zoom_start=12, max_zoom=25, 
+                  zoom_start=12, max_zoom=25, min_zoom=10
                   min_lat=47.0, max_lat=48.0, 
                   min_lon=-123.0, max_lon=-121.0):
-    m = folium.Map(location=location, zoom_start=zoom_start, max_zoom=max_zoom, min_lat=min_lat, 
-                   max_lat=max_lat, min_lon=min_lon, max_lon=max_lon, max_bounds=True, control_scale=True)
+    m = folium.Map(location=location, zoom_start=zoom_start, max_zoom=max_zoom, min_zoom=min_zoom,
+                   min_lat=min_lat, max_lat=max_lat, min_lon=min_lon, max_lon=max_lon, max_bounds=True, control_scale=True)
     base_layer = folium.TileLayer(tiles=tiles, attr=attr, name=map_name, overlay=overlay, control=control)
     base_layer.add_to(m)
     return m
